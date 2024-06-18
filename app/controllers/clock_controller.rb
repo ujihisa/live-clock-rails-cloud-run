f5d644138b526e2eea6825eba5cbed52103ddae5
ujihisa <ujihisa@users.noreply.github.com>
2024-06-17 22:33:02 -0700

    Copy from https://socketry.github.io/live/guides/rails-integration/index

---
+++ app/controllers/clock_controller.rb
@@ -0,0 +1,40 @@
+require 'async/websocket/adapters/rails'
+
+class ClockTag < Live::Tag
+  def initialize(name)
+    @name = name
+  end
+
+  def bind(page)
+    super
+
+    # Schedule a refresh every second:
+    Async do
+      while true
+        sleep 1
+        replace!
+      end
+    end
+  end
+
+  def render(builder)
+    builder.tag('div') do
+      builder.text(Time.now.to_s)
+    end
+  end
+end
+
+class ClockController < ApplicationController
+  RESOLVER = Live::Resolver.allow(ClockTag)
+
+  def index
+    @tag = ClockTag.new('flappy')
+  end
+
+  skip_before_action :verify_authenticity_token, only: :live
+
+  def live
+    self.response = Async::WebSocket::Adapters::Rails.open(request) do |connection|
+      Live::Page.new(RESOLVER).run(connection)
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root 'clock/index'
+  match "clock/live", via: [:get, :connect]
 end
