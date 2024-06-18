6f81770a1bf788611e0b2a3b4050bfe4a21afa17
ujihisa <ujihisa@users.noreply.github.com>
2024-06-18 16:16:39 -0700

    Copy from https://github.com/socketry/flappy-bird/blob/3b7119aac4a4c04317d47d0b6d62575b048e10b1/config/importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "morphdom" # @2.7.2
+pin "@socketry/live", to: "@socketry--live.js" # @0.13.0
